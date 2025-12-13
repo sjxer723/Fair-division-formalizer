c4f8246e4e3f2b5f0b4b7184c69a9294f826db6e
Jiaxin Song <sjtu_xiaosong@sjtu.edu.cn>
2025-12-13 13:51:35 -0600

    initial setup

---
+++ Efx.lean
@@ -0,0 +1,6 @@
+import Efx.Basic
+import Mathlib
+import Mathlib.Data.Set.Basic
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Fintype.Basic
+import Mathlib.Tactic
---
+++ Efx/most_envious.lean
@@ -0,0 +1,8 @@
+import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Fintype.Basic
+import Mathlib.Tactic
+import Efx.Basic
+
+open FairDivision
+open Finset
+open Function
